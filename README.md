58112740e3773177856d59b884b645e19e807a11
patree <treeshnapati@gmail.com>
2023-03-29 17:55:57 -0500

    Add base.html creation to web app structure

--- README.md
+++ README.md
@@ -2,19 +2,9 @@
 
 `PyBlueprintMaker` is a Python-based project scaffolding tool that helps you create well-organized project structures using best-practices, `fixtures`, `pytest`, and the Python Standard Library. With support for `basic` to `modular` folder configurations and comprehensive testing, PyBlueprintMaker is the ultimate solution for maintainable projects.
 
-PyBlueprintMaker is easy to use and provides everything you need to get started quickly. Whether you're a seasoned developer or just getting started, PyBlueprintMaker is the perfect tool to help you streamline your workflow and create high-quality projects with ease.
-
-With PyBlueprintMaker, you can create project structures that are optimized for performance, scalability, and maintainability. Our tool provides support for a wide range of configurations, including basic, intermediate, advanced, and extended folder structures, so you can choose the one that's right for your project.
-
-PyBlueprintMaker also includes comprehensive testing support, with built-in pytest fixtures and support for all major testing frameworks. Our tool is fully customizable, with support for a wide range of plugins and add-ons that can help you take your projects to the next level.
-
-So what are you waiting for? Try PyBlueprintMaker today and see how easy it is to create high-quality, maintainable projects in Python!
-
-![scafolds](assets/scaffolds.jpeg)
-
 ## Why is File Structure Important?
 
-A well-organized file structure is crucial for any software project. It makes your code easier to understand, navigate, and maintain. Following best practices for file structure ensures consistency across projects, which can be especially helpful when **collaborating** with others or when you revisit your project after some time.
+A well-organized file structure is crucial for any software project. It makes your code easier to understand, navigate, and maintain. Following best practices for file structure ensures consistency across projects, which can be especially helpful when collaborating with others or when you revisit your project after some time.
 
 ## Best Practices
 
@@ -28,25 +18,43 @@ Some best practices for organizing Python projects include:
 
 ## Installation
 
-You can install PyBlueprintMaker via `pip`:
+To install PyBlueprintMaker, you can use pip:
 
-```bash
+```
 pip install PyBlueprintMaker
 ```
+
+Alternatively, you can install from source by cloning this repository and running:
+
+```
+python setup.py install
+```
+
+Make sure to also include any necessary dependencies that are not automatically installed.
 
 ## Usage
 
 To use PyBlueprintMaker, run the following command:
 
-`PyBlueprintMaker [output_path] [structure]`
+```
+PyBlueprintMaker [output_path] [structure]
+```
 
-Where output_path is the desired output path for the project structure and structure is the project structure to create. The supported structures are basic, intermediate, advanced, extended, or modular.
+Where `output_path` is the desired output path for the project structure and structure is the project structure to create. The supported structures are` basic`, `intermediate`, `advanced`, `extended`, or `modular`.
 
-For example, to create a basic project structure in a folder named `my_project`, run the following command:
+For example, to create a `basic` project structure in a folder named `my_project`, run the following command:
 
 ```
 PyBlueprintMaker my_project basic
 ```
+
+## Outputs and Structures
+
+PyBlueprintMaker supports the following project structures:
+
+- `basic`: This structure is intended for simple projects with a single Python script and a `README.md` file.
+
+These folder structures are designed to accommodate various aspects of data architecture and data science projects, including data storage, version control, automated testing, containerization, and reproducibility. The most advanced structure is intended for projects with complex configurations and additional tools for automation and collaboration.
 
 ## Contributing
 
@@ -54,10 +62,10 @@ If you find any issues or have suggestions, feel free to open an issue or submit
 
 ## License
 
-PyBlueprintMaker is licensed under the [MIT License](LICENSE).
+PyBlueprintMaker is licensed under the MIT License.
 
 ## Support
 
-If you like this project, please consider sharing a coffee. ☕
+If you like this project, please consider sharing a coffee. Thank you! 🦉
 
 <a href="https://www.buymeacoffee.com/patimejiaS" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
-import os
+from src.main import create_etl_structure, create_modular_structure, create_ml_project_structure, create_web_app_structure
 from pathlib import Path
-import pytest
 import shutil
-from src.main import create_basic_structure, create_intermediate_structure, create_advanced_structure, create_extended_structure, create_modular_structure
+import os
+import pytest
 
 @pytest.fixture
 def output_path(tmpdir):
@@ -10,64 +10,27 @@ def output_path(tmpdir):
     yield path
     shutil.rmtree(str(path))
 
-def test_create_basic_structure(output_path: Path):
-    create_basic_structure(str(output_path))
+def test_create_etl_structure(output_path: Path):
+    create_etl_structure(str(output_path))
     assert (output_path / "src").is_dir()
     assert (output_path / "tests").is_dir()
-    assert (output_path / "src" / "main.py").is_file()
-    assert (output_path / "tests" / "test_main.py").is_file()
-    assert (output_path / "README.md").is_file()
-
-
-def test_create_intermediate_structure(output_path: Path):
-    create_intermediate_structure(str(output_path))
-    assert (output_path / "src").is_dir()
-    assert (output_path / "tests").is_dir()
-    assert (output_path / "src" / "main.py").is_file()
-    assert (output_path / "tests" / "test_main.py").is_file()
-    assert (output_path / "src" / "utils").is_dir()
-    assert (output_path / "src" / "utils" / "helpers.py").is_file()
-    assert (output_path / "tests" / "test_helpers.py").is_file()
-    assert (output_path / "README.md").is_file()
-
-def test_create_advanced_structure(output_path: Path):
-    create_advanced_structure(str(output_path))
-    assert (output_path / "src").is_dir()
-    assert (output_path / "tests").is_dir()
-    assert (output_path / "src" / "main.py").is_file()
-    assert (output_path / "tests" / "test_main.py").is_file()
-    assert (output_path / "src" / "utils").is_dir()
-    assert (output_path / "src" / "utils" / "helpers.py").is_file()
-    assert (output_path / "tests" / "test_helpers.py").is_file()
-    assert (output_path / "src" / "services").is_dir()
-    assert (output_path / "src" / "services" / "service.py").is_file()
-    assert (output_path / "tests" / "test_service.py").is_file()
-    assert (output_path / "README.md").is_file()
-
-def test_create_extended_structure(output_path: Path):
-    create_extended_structure(str(output_path))
-    assert (output_path / "src").is_dir()
-    assert (output_path / "tests").is_dir()
-    assert (output_path / "src" / "main.py").is_file()
-    assert (output_path / "tests" / "test_main.py").is_file()
-    assert (output_path / "src" / "utils").is_dir()
-    assert (output_path / "src" / "utils" / "helpers.py").is_file()
-    assert (output_path / "tests" / "test_helpers.py").is_file()
-    assert (output_path / "src" / "services").is_dir()
-    assert (output_path / "src" / "services" / "service.py").is_file()
-    assert (output_path / "tests" / "test_service.py").is_file()
-    assert (output_path / "assets").is_dir()
     assert (output_path / "data").is_dir()
-    assert (output_path / "assets" / "images").is_dir()
+    assert (output_path / "src" / "extract").is_dir()
+    assert (output_path / "src" / "transform").is_dir()
+    assert (output_path / "src" / "load").is_dir()
     assert (output_path / "data" / "input").is_dir()
     assert (output_path / "data" / "intermediate").is_dir()
     assert (output_path / "data" / "output").is_dir()
+    assert (output_path / "src" / "main.py").is_file()
+    assert (output_path / "tests" / "test_extract.py").is_file()
+    assert (output_path / "tests" / "test_transform.py").is_file()
+    assert (output_path / "tests" / "test_load.py").is_file()
     assert (output_path / "README.md").is_file()
+    assert (output_path / "requirements.txt").is_file()
 
 def test_create_modular_structure(output_path: Path):
     module_names = ["utils", "services", "models"]
     create_modular_structure(str(output_path), module_names)
-
     assert (output_path / "src").is_dir()
     assert (output_path / "tests").is_dir()
     assert (output_path / "src" / "__init__.py").is_file()
@@ -75,7 +38,55 @@ def test_create_modular_structure(output_path: Path):
     assert (output_path / "requirements.txt").is_file()
 
     for module_name in module_names:
-        module_path = os.path.join(output_path, "src", module_name)
-        assert os.path.isdir(module_path)
-        assert os.path.isfile(os.path.join(module_path, "__init__.py"))
-        assert os.path.isfile(os.path.join(module_path, f"{module_name}.py"))
+        module_path = output_path / "src" / module_name
+        assert (module_path).is_dir()
+        assert (module_path / "__init__.py").is_file()
+        assert (module_path / f"{module_name}.py").is_file()
+
+        test_file_path = output_path / "tests" / f"test_{module_name}.py"
+        assert (test_file_path).is_file()
+
+def test_create_ml_project_structure(output_path: Path):
+    create_ml_project_structure(str(output_path))
+    assert (output_path / "src").is_dir()
+    assert (output_path / "tests").is_dir()
+    assert (output_path / "data").is_dir()
+    assert (output_path / "src" / "data").is_dir()
+    assert (output_path / "src" / "models").is_dir()
+    assert (output_path / "src" / "main.py").is_file()
+    assert (output_path / "src" / "data" / "preprocess.py").is_file()
+    assert (output_path / "src" / "data" / "features.py").is_file()
+    assert (output_path / "src" / "models" / "model.py").is_file()
+    assert (output_path / "src" / "models" / "train.py").is_file()
+    assert (output_path / "src" / "models" / "evaluate.py").is_file()
+    assert (output_path / "tests" / "test_preprocess.py").is_file()
+    assert (output_path / "tests" / "test_features.py").is_file()
+    assert (output_path / "tests" / "test_model.py").is_file()
+    assert (output_path / "tests" / "test_train.py").is_file()
+    assert (output_path / "tests" / "test_evaluate.py").is_file()
+    assert (output_path / "data" / "raw").is_dir()
+    assert (output_path / "data" / "interim").is_dir()
+    assert (output_path / "data" / "processed").is_dir()
+    assert (output_path / "data" / "models").is_dir()
+    assert (output_path / "README.md").is_file()
+    assert (output_path / "requirements.txt").is_file()
+
+def test_create_web_app_structure(output_path: Path):
+    create_web_app_structure(str(output_path))
+    assert (output_path / "src").is_dir()
+    assert (output_path / "tests").is_dir()
+    assert (output_path / "static").is_dir()
+    assert (output_path / "templates").is_dir()
+    assert (output_path / "src" / "main.py").is_file()
+    assert (output_path / "src" / "views.py").is_file()
+    assert (output_path / "src" / "forms.py").is_file()
+    assert (output_path / "src" / "models.py").is_file()
+    assert (output_path / "tests" / "test_views.py").is_file()
+    assert (output_path / "tests" / "test_forms.py").is_file()
+    assert (output_path / "tests" / "test_models.py").is_file()
+    assert (output_path / "static" / "css").is_dir()
+    assert (output_path / "static" / "js").is_dir()
+    assert (output_path / "static" / "images").is_dir()
+    assert (output_path / "templates" / "base.html").is_file()
+    assert (output_path / "README.md").is_file()
+    assert (output_path / "requirements.txt").is_file()
